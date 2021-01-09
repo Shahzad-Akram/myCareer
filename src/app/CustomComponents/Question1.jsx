4e0cdbdcba81bf021afea87838debbda7c83c64e
Naeemkhan33 <ranker333@gmail.com>
2021-01-09 05:20:43 +0500

    styles steps

---
+++ src/app/CustomComponents/Question1.jsx
@@ -0,0 +1,198 @@
+import React from 'react';
+
+const Question1 = () => {
+  return (
+    <div>
+      <h4 className='font-weight-bold mb-6'>
+        Question 1: Lorem ipsum dolor site amet
+      </h4>
+      <div className='d-flex justify-content-between mb-8'>
+        <div className='d-flex align-items-center'>
+          <span className='mr-4'>
+            <img
+              className='rounded-lg'
+              style={{
+                objectFit: 'cover',
+              }}
+              height={70}
+              width={100}
+              src='https://source.unsplash.com/random'
+              alt='pic'
+            />
+          </span>
+          <span>
+            <h6 className='font-weight-bold'>Idle & Empty</h6>
+            <span className='text-black-50'>Lorem ipsum dolor sit amet.</span>
+          </span>
+        </div>
+        <div>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
+          >
+            <b>1</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-warning text-hover-white mr-2'
+          >
+            <b>2</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
+          >
+            <b>3</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white'
+          >
+            <b>4</b>
+          </button>
+        </div>
+      </div>
+      {/* -- */}
+      <div className='d-flex justify-content-between mb-8'>
+        <div className='d-flex align-items-center'>
+          <span className='mr-4'>
+            <img
+              className='rounded-lg'
+              style={{
+                objectFit: 'cover',
+              }}
+              height={70}
+              width={100}
+              src='https://source.unsplash.com/random'
+              alt='pic'
+            />
+          </span>
+          <span>
+            <h6 className='font-weight-bold'>Idle & Empty</h6>
+            <span className='text-black-50'>Lorem ipsum dolor sit amet.</span>
+          </span>
+        </div>
+        <div>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
+          >
+            <b>1</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-warning text-hover-white mr-2'
+          >
+            <b>2</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
+          >
+            <b>3</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white'
+          >
+            <b>4</b>
+          </button>
+        </div>
+      </div>
+      <div className='d-flex justify-content-between mb-8'>
+        <div className='d-flex align-items-center'>
+          <span className='mr-4'>
+            <img
+              className='rounded-lg'
+              style={{
+                objectFit: 'cover',
+              }}
+              height={70}
+              width={100}
+              src='https://source.unsplash.com/random'
+              alt='pic'
+            />
+          </span>
+          <span>
+            <h6 className='font-weight-bold'>Idle & Empty</h6>
+            <span className='text-black-50'>Lorem ipsum dolor sit amet.</span>
+          </span>
+        </div>
+        <div>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
+          >
+            <b>1</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-warning text-hover-white mr-2'
+          >
+            <b>2</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
+          >
+            <b>3</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white'
+          >
+            <b>4</b>
+          </button>
+        </div>
+      </div>
+      <div className='d-flex justify-content-between mb-8'>
+        <div className='d-flex align-items-center'>
+          <span className='mr-4'>
+            <img
+              className='rounded-lg'
+              style={{
+                objectFit: 'cover',
+              }}
+              height={70}
+              width={100}
+              src='https://source.unsplash.com/random'
+              alt='pic'
+            />
+          </span>
+          <span>
+            <h6 className='font-weight-bold'>Idle & Empty</h6>
+            <span className='text-black-50'>Lorem ipsum dolor sit amet.</span>
+          </span>
+        </div>
+        <div>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
+          >
+            <b>1</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-warning text-hover-white mr-2'
+          >
+            <b>2</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
+          >
+            <b>3</b>
+          </button>
+          <button
+            type='button'
+            className='btn btn-bg-light btn-hover-bg-warning text-hover-white'
+          >
+            <b>4</b>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Question1;
--- src/app/pages/Dashboard2Page.jsx
+++ src/app/pages/Dashboard2Page.jsx
@@ -1,224 +1,44 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import MobileStepper from '@material-ui/core/MobileStepper';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '90%',
-  },
-  backButton: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-}));
+import Question1 from '../CustomComponents/Question1';
 
 function getSteps() {
-  return [
-    'Select master blaster campaign settings',
-    'Create an ad group',
-    'Create an ad',
-  ];
+  return ['1', '2', '3', '4', '5', '6', '7'];
 }
 
-const Question1 = () => {
+function getStepsOne() {
+  return ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
+}
+
+const StepperTwo = ({ activeStepOne, onClick, onBack }) => {
   return (
-    <div className='bg-white p-8 rounded-lg mt-10'>
-      <h4 className='font-weight-bold mb-6'>
-        Question 1: Lorem ipsum dolor site amet
-      </h4>
-      <div className='d-flex justify-content-between mb-8'>
-        <div className='d-flex align-items-center'>
-          <span className='mr-4'>
-            <img
-              className='rounded-lg'
-              style={{
-                objectFit: 'cover',
-              }}
-              height={70}
-              width={100}
-              src='https://source.unsplash.com/random'
-              alt='pic'
-            />
-          </span>
-          <span>
-            <h6 className='font-weight-bold'>Idle & Empty</h6>
-            <span className='text-black-50'>Lorem ipsum dolor sit amet.</span>
-          </span>
-        </div>
-        <div>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
-          >
-            <b>1</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-warning text-hover-white mr-2'
-          >
-            <b>2</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
-          >
-            <b>3</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white'
-          >
-            <b>4</b>
-          </button>
-        </div>
-      </div>
-      {/* -- */}
-      <div className='d-flex justify-content-between mb-8'>
-        <div className='d-flex align-items-center'>
-          <span className='mr-4'>
-            <img
-              className='rounded-lg'
-              style={{
-                objectFit: 'cover',
-              }}
-              height={70}
-              width={100}
-              src='https://source.unsplash.com/random'
-              alt='pic'
-            />
-          </span>
-          <span>
-            <h6 className='font-weight-bold'>Idle & Empty</h6>
-            <span className='text-black-50'>Lorem ipsum dolor sit amet.</span>
-          </span>
-        </div>
-        <div>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
-          >
-            <b>1</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-warning text-hover-white mr-2'
-          >
-            <b>2</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
-          >
-            <b>3</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white'
-          >
-            <b>4</b>
-          </button>
-        </div>
-      </div>
-      <div className='d-flex justify-content-between mb-8'>
-        <div className='d-flex align-items-center'>
-          <span className='mr-4'>
-            <img
-              className='rounded-lg'
-              style={{
-                objectFit: 'cover',
-              }}
-              height={70}
-              width={100}
-              src='https://source.unsplash.com/random'
-              alt='pic'
-            />
-          </span>
-          <span>
-            <h6 className='font-weight-bold'>Idle & Empty</h6>
-            <span className='text-black-50'>Lorem ipsum dolor sit amet.</span>
-          </span>
-        </div>
-        <div>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
-          >
-            <b>1</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-warning text-hover-white mr-2'
-          >
-            <b>2</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
-          >
-            <b>3</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white'
-          >
-            <b>4</b>
-          </button>
-        </div>
-      </div>
-      <div className='d-flex justify-content-between mb-8'>
-        <div className='d-flex align-items-center'>
-          <span className='mr-4'>
-            <img
-              className='rounded-lg'
-              style={{
-                objectFit: 'cover',
-              }}
-              height={70}
-              width={100}
-              src='https://source.unsplash.com/random'
-              alt='pic'
-            />
-          </span>
-          <span>
-            <h6 className='font-weight-bold'>Idle & Empty</h6>
-            <span className='text-black-50'>Lorem ipsum dolor sit amet.</span>
-          </span>
-        </div>
-        <div>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
-          >
-            <b>1</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-warning text-hover-white mr-2'
-          >
-            <b>2</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white mr-2'
-          >
-            <b>3</b>
-          </button>
-          <button
-            type='button'
-            className='btn btn-bg-light btn-hover-bg-warning text-hover-white'
-          >
-            <b>4</b>
-          </button>
-        </div>
-      </div>
-    </div>
+    <>
+      <MobileStepper
+        className='justify-content-start'
+        variant='progress'
+        steps={7}
+        position='static'
+        activeStep={activeStepOne}
+        backButton={
+          <div className='d-flex align-items-center text-black-50 mr-3'>
+            <span className='p-0' onClick={onBack}>
+              1
+            </span>
+            <span>/</span>
+            <span className='p-0' onClick={onClick}>
+              7
+            </span>
+          </div>
+        }
+      />
+    </>
   );
 };
 
@@ -227,18 +47,29 @@ function getStepContent(stepIndex) {
     case 0:
       return <Question1 />;
     case 1:
-      return 'What is an ad group anyways?';
+      return <Question1 />;
     case 2:
-      return 'This is the bit I really care about!';
+      return <Question1 />;
+    case 3:
+      return <Question1 />;
+    case 4:
+      return <Question1 />;
+    case 5:
+      return <Question1 />;
+    case 6:
+      return <Question1 />;
+    case 7:
+      return <Question1 />;
     default:
       return 'Uknown stepIndex';
   }
 }
 
 export const Dashboard2Page = () => {
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
+  const [activeStep, setActiveStep] = useState(0);
+  const [activeStepOne, setActiveStepOne] = useState(0);
   const steps = getSteps();
+  const stepsOne = getStepsOne();
 
   function handleNext() {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -259,42 +90,60 @@ export const Dashboard2Page = () => {
         paddingTop: 70,
       }}
     >
-      <div className={classes.root}>
-        <Stepper activeStep={activeStep} alternativeLabel>
-          {steps.map((label) => (
+      <div className='mr-12'>
+        <Stepper
+          className='rounded-lg'
+          activeStep={activeStepOne}
+          alternativeLabel
+        >
+          {stepsOne.map((label) => (
             <Step key={label}>
               <StepLabel>{label}</StepLabel>
+              <div className='small mt-4 text-center'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Sapiente sunt a nesciunt aspernatur nostrum eveniet.
+              </div>
+              <div className='text-center mt-4'>
+                <span className='badge badge-light'>0/4 Completed</span>
+              </div>
+              <div className='text-center mt-4'>
+                <span className='badge badge-danger'>Paid</span>
+              </div>
             </Step>
           ))}
         </Stepper>
         <div>
           {activeStep === steps.length ? (
             <div>
-              <Typography className={classes.instructions}>
-                All steps completed
-              </Typography>
+              <Typography>All steps completed</Typography>
+
               <Button onClick={handleReset}>Reset</Button>
             </div>
           ) : (
-            <div>
-              <Typography className={classes.instructions}>
-                {getStepContent(activeStep)}
-              </Typography>
-              <div>
-                <Button
-                  disabled={activeStep === 0}
-                  onClick={handleBack}
-                  className={classes.backButton}
-                >
-                  Back
-                </Button>
-                <Button
-                  variant='contained'
-                  color='primary'
-                  onClick={handleNext}
-                >
-                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                </Button>
+            <div className='bg-white p-8 rounded-lg mt-10'>
+              <Typography>{getStepContent(activeStep)}</Typography>
+              <div className='row justify-content-between'>
+                <div className='col-12 col-md-3'>
+                  <StepperTwo
+                    activeStepOne={activeStep}
+                    onClick={handleNext}
+                    disabled={activeStep === 7}
+                    onBack={handleBack}
+                    disabled={activeStep === 0}
+                  />
+                </div>
+                <div className='col-12 col-md-3 text-right'>
+                  <Button
+                    className='btn-primary'
+                    variant='contained'
+                    color='primary'
+                    onClick={handleNext}
+                  >
+                    {activeStepOne === steps.length - 1
+                      ? 'Finish'
+                      : 'Next Question'}
+                  </Button>
+                </div>
               </div>
             </div>
           )}
